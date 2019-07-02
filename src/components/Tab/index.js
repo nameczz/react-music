fa403f60d269ceec59fd814cb949dfdc75affcb4
zizhao <zizhao@zamplus.com>
2019-07-02 14:54:28 +0800

    recommend page

--- src/components/Tab/index.js
+++ src/components/Tab/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink, withRouter } from 'react-router-dom'
 import { Tabs, WhiteSpace } from 'antd-mobile'
 import './index.styl'
 
@@ -9,14 +10,39 @@ const tabs = [
   { title: '搜索', key: 'search' }
 ]
 const activeColor = '#ffcd32'
+@withRouter
 class Tab extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      activeIndex: 0
+    }
+  }
   handleTabChange = (tab, index) => {
     console.log('onChange', index, tab)
     console.log(this)
   }
+
+  activeTab = (match, location) => {
+    console.log(match, location)
+    if (!match) {
+      return false
+    }
+
+    return true
+  }
+
+  componentWillMount() {
+    const pathname = this.props.location.pathname
+    const index = tabs.findIndex(t => pathname.includes(t.key))
+    this.setState({
+      activeIndex: index === -1 ? 0 : index
+    })
+  }
+
   render() {
     return (
-      <div>
+      <div className="tab-wrapper">
         <Tabs
           tabBarUnderlineStyle={{
             border: `1px ${activeColor} solid`
@@ -24,8 +50,17 @@ class Tab extends React.Component {
           tabBarActiveTextColor={activeColor}
           tabBarBackgroundColor="#222"
           tabs={tabs}
-          initialPage={0}
-          renderTab={tab => <span>{tab.title}</span>}
+          initialPage={this.state.activeIndex}
+          renderTab={tab => (
+            <NavLink
+              to={`/${tab.key}`}
+              activeClassName="active-tab"
+              isActive={this.activeTab}
+              style={{ width: '100%', textAlign: 'center' }}
+            >
+              {tab.title}
+            </NavLink>
+          )}
           onChange={this.handleTabChange}
         />
         <WhiteSpace />
--- src/pages/Recommend/index.js
+++ src/pages/Recommend/index.js
@@ -1,23 +1,56 @@
 /* eslint no-dupe-keys: 0, no-mixed-operators: 0 */
 import React from 'react'
-import { List, Carousel } from 'antd-mobile'
+import { Carousel, ListView } from 'antd-mobile'
 import { getRecommend, getdiscList } from 'api/recommend'
 import { ERR_OK } from 'api/config'
 
 import './index.styl'
-import ListItem from 'antd-mobile/lib/list/ListItem'
+
+function genData(pIndex = 0, NUM_ROWS = 0) {
+  const dataBlob = {}
+  for (let i = 0; i < NUM_ROWS; i++) {
+    const ii = pIndex * NUM_ROWS + i
+    dataBlob[`${ii}`] = `row - ${ii}`
+  }
+  return dataBlob
+}
+function MyBody(props) {
+  return (
+    <div className="am-list-body my-body">
+      <span style={{ display: 'none' }}>you can custom body wrap element</span>
+      {props.children}
+    </div>
+  )
+}
 
 class Recommend extends React.Component {
   constructor(props) {
     super(props)
+
+    const dataSource = new ListView.DataSource({
+      rowHasChanged: (row1, row2) => row1 !== row2
+    })
+
     this.state = {
+      dataSource,
+      isLoading: true,
       recommends: [],
-      discList: []
+      discList: [],
+      height: document.documentElement.clientHeight - 94,
+      width: document.documentElement.clientWidth
     }
   }
   componentDidMount() {
     this.getRecommend()
     this.getDiscList()
+  }
+
+  setDataSource = () => {
+    this.rData = genData(0, this.state.discList.length)
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(this.rData),
+      isLoading: false
+    })
   }
 
   getRecommend = () => {
@@ -34,50 +67,82 @@ class Recommend extends React.Component {
     getdiscList().then(res => {
       console.log(res)
       if (res.code === ERR_OK) {
-        this.setState({
-          discList: res.data.list
-        })
+        this.setState(
+          {
+            discList: res.data.list
+          },
+          this.setDataSource
+        )
       }
     })
   }
   render() {
     const listHeader = () => {
-      return <h1 className="list-title">热门歌单推荐</h1>
+      return (
+        <div style={{ width: this.state.width }}>
+          {this.state.recommends.length > 0 ? (
+            <Carousel infinite>
+              {this.state.recommends.map(music => {
+                return (
+                  <a key={music.id} href={music.linkUrl}>
+                    <img
+                      className="recommend-img"
+                      alt="recommend "
+                      src={music.picUrl}
+                      onLoad={() => {
+                        // fire window resize event to change height
+                        window.dispatchEvent(new Event('resize'))
+                        this.setState({ imgHeight: 'auto' })
+                      }}
+                    />
+                  </a>
+                )
+              })}
+            </Carousel>
+          ) : null}
+
+          <h1 className="list-title">热门歌单推荐</h1>
+        </div>
+      )
+    }
+
+    let index = 0
+    const row = (rowData, sectionID, rowID) => {
+      if (!this.state.discList.length) {
+        return <div>nothing</div>
+      }
+      if (index > this.state.length - 1) {
+        index = 0
+      }
+      const obj = this.state.discList[index++]
+      return (
+        <div className="item" key={rowID}>
+          <div className="icon">
+            <img alt="song" width="60" height="60" src={obj.imgurl} />
+          </div>
+          <div className="text">
+            <h2 className="name">{obj.creator.name}</h2>
+            <p className="desc">{obj.dissname}</p>
+          </div>
+        </div>
+      )
     }
     return (
-      <div className="recommend">
-        <Carousel infinite>
-          {this.state.recommends.map(music => {
-            return (
-              <a key={music.id} href={music.linkUrl}>
-                <img
-                  className="recommend-img"
-                  alt="recommend "
-                  src={music.picUrl}
-                  onLoad={() => {
-                    // fire window resize event to change height
-                    window.dispatchEvent(new Event('resize'))
-                    this.setState({ imgHeight: 'auto' })
-                  }}
-                />
-              </a>
-            )
-          })}
-        </Carousel>
-
-        <List renderHeader={listHeader} className="recommend-list">
-          {this.state.discList.map(song => {
-            return (
-              <ListItem className="item" thumb={song.imgurl}>
-                <div className="text">
-                  <h2 className="name">{song.creator.name}</h2>
-                  <p className="desc">{song.dissname}</p>
-                </div>
-              </ListItem>
-            )
-          })}
-        </List>
-      </div>
+      <ListView
+        ref={el => (this.lv = el)}
+        dataSource={this.state.dataSource}
+        renderHeader={listHeader}
+        renderBodyComponent={() => <MyBody />}
+        renderRow={row}
+        style={{
+          height: this.state.height,
+          overflow: 'auto'
+        }}
+        onScroll={() => {
+          console.log('scroll')
+        }}
+        scrollRenderAheadDistance={500}
+      />
     )
   }
 }
